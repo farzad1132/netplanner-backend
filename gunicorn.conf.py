3ff7b945cac65f24e34562b987533702a05de239
farzad mohammadi <farzad.mohammadi87@gmail.com>
2020-09-26 19:36:40 +0330

    edit on gunicorn configuration

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,6 @@ import app
 bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = "gunicorn_access_log.txt"
-access_log_format = %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"
 errorlog =  "gunicorn_error_log.txt"
 
 # TODO: specify workers type
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from app import connex_app
 
+# Running app with gunicorn:
+# in Terminal:
+#   gunicorn -c gunicorn.conf.py wsgi:connex_app
+#   watch access and error logs for more information
+
 if __name__ == "__main__":
     connex_app.run()
