6a4acd55913e741c68efcc878d5399abc5217871
farzad.mohammadi87 <farzad.mohammadi87@gmail.com>
2020-10-11 23:05:26 +0330

    hiding db settings

--- config.py
+++ config.py
@@ -11,14 +11,19 @@ connex_app = connexion.App(__name__, specification_dir=os.path.join(basedir, "op
 
 # Get the underlying Flask app instance
 app = connex_app.app
+DB = os.environ["DB"]
+DB_PORT = os.environ["DB_PORT"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PASS = os.environ["DB_PASS"]
+DB_USER = os.environ["DB_USER"]
 
-# Build the Sqlite ULR for SqlAlchemy
-postgresql_url = 'postgresql://postgres:1234@localhost:5433/netplanner'
+db_url = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB}"
+
 
 # Configure the SqlAlchemy part of the app instance
 app.config["SECRET_KEY"] = '61837f3a27abb1e4b88a47d0502cf2e604071378a1e9ec4c42eafbba933180a7'
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = postgresql_url
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Create the SqlAlchemy db instance
