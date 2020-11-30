e17455e08b8cb709abc2276810ff4e8bffcf5947
farzad.mohammadi87 <farzad.mohammadi87@gmail.com>
2020-11-30 19:59:44 +0330

    error checking added to post and put methods of pt

--- PhysicalTopology.py
+++ PhysicalTopology.py
@@ -41,6 +41,37 @@ PHYSICALTOPOLOGY = {
     ]
 }
 
+def check_pt_format(pt):
+    # this function is used for error checking in physical topology
+    # it also adds '<property>_error' properties in case that finds an error
+
+    flag = True
+    nodes_name_list = []
+    for node in pt["nodes"]:
+        nodes_name_list.append(node["name"])
+        if not isinstance(node["lat"], float):
+            flag = False
+            node["lat_error"] = "err_code:1, 'lat' must be float"
+        if not isinstance(node["lng"], float):
+            flag = False
+            node["lng_error"] = "err_code:1, 'lat' must be float"
+        if not node["roadm_type"] in ("Directionless", "CDC"):
+            flag = False
+            node["roadm_type_error"] = "err_code:2, roadm_type must be from ('Directionless','CDC')"
+    
+    for link in pt["links"]:
+        if not link["source"] in nodes_name_list:
+            flag = False
+            link["source_error"] = "err_code:3, link 'source' must be one of the nodes"
+        if not link["destination"] in nodes_name_list:
+            flag = False
+            link["destination_error"] = "err_code:3, link 'destination' must be one of the nodes"
+        if not (isinstance(link["distance"], int) or isinstance(link["distance"], float)):
+            flag = False
+            link["distance_error"] = "err_code:4, 'distance' must be float or integer"
+    
+    return flag
+
 def get_physical_topology(id, user_id, version=None):
     # this endpoint will send a physical topology to front
     # if version is specified then this endpoint will only return that version but if version is not specified
@@ -71,6 +102,8 @@ def get_physical_topology(id, user_id, version=None):
 
 def create_physical_topology(body, user_id):
     # this endpoint creates a record in physical topology database with received object
+    # NOTE: this endpoint will check physical topology and if it finds and error it will return a JSOM
+    #       like from_excel endpoint ( and its err_codes ).
     #
     # Parameters:
     #   1. user_id
@@ -97,9 +130,11 @@ def create_physical_topology(body, user_id):
     if UserModel.query.filter_by(id=user_id).one_or_none() is None:
         return {"error_msg": f"user with id = {user_id} not found"}, 404
 
-    id = uuid.uuid4().hex
+    if not check_pt_format(physical_topology):
+        return {"error_msg": "there is/are error(s) in physical toplogy", "pt": physical_topology}, 400
+
     pt_object = PhysicalTopologyModel(name=name, data=physical_topology, comment=comment, 
-                                        id=id, version=1)
+                                        version=1)
     pt_object.user_id = user_id
     db.session.add(pt_object)
     db.session.commit()
@@ -108,6 +143,7 @@ def create_physical_topology(body, user_id):
 
 def update_physical_topology(body, user_id):
     # this endpoint will update a physical topology
+    # NOTE: this endpoint has error checking (like create_physical toplogy and from_excel endpoints)
     #
     # parameters:
     #   1. user_id
@@ -133,6 +169,9 @@ def update_physical_topology(body, user_id):
     
     if (new_pt:=body["physical_topology"]) is None:
         return {"error_msg": "'physical topology' can not be None"}, 400
+    
+    if not check_pt_format(new_pt):
+        return {"error_msg": "there is/are error(s) in physical toplogy", "pt": new_pt}, 400
 
     if (user:=UserModel.query.filter_by(id=user_id).one_or_none()) is None:
         return {"error_msg": f"user with id = {user_id} not found"}, 404
@@ -212,10 +251,10 @@ def read_from_excel(body, pt_binary, user_id):
     #       in that item explaining problem along with error code.
     #
     # error codes in this endpoint:
-    #   1. X must be float
-    #   2. X must be from ('Directionless','CDC')
-    #   3. X must be one of the nodes
-    #   4. X must be float for integer
+    #   err_code:1. X must be float
+    #   err_code:2. X must be from ('Directionless','CDC')
+    #   err_code:3. X must be one of the nodes
+    #   err_code:4. X must be float for integer
 
     if (name:=body["name"]) is None:
         return {"error_msg": "'name' can not be None"}, 400
