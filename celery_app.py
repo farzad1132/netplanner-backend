c50ad04fac82afa42c249322362ca2d324f35329
farzad.mohammadi87 <farzad.mohammadi87@gmail.com>
2020-12-31 12:37:24 +0330

    env variable edit

--- celery_app.py
+++ celery_app.py
@@ -3,11 +3,8 @@ import os
 
 BROKER_DEFAULT_USER = os.environ.get("BROKER_DEFAULT_USER")
 BROKER_DEFAULT_PASSWORD = os.environ.get("BROKER_DEFAULT_PASSWORD")
-#BROKER_HOST = os.environ.get("BROKER_HOST")
-#BROKER_PORT = os.environ.get("BROKER_PORT")
-
-BROKER_HOST = "localhost"
-BROKER_PORT = 5672
+BROKER_HOST = os.environ.get("BROKER_HOST")
+BROKER_PORT = os.environ.get("BROKER_PORT")
 
 broker_url = f'amqp://{BROKER_DEFAULT_USER}:{BROKER_DEFAULT_PASSWORD}@{BROKER_HOST}:{BROKER_PORT}/'
 
