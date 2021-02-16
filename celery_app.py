c9e9e82b054c1556f40d1def23657f390028f246
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-02-16 20:09:26 +0330

    removing template worker

--- celery_app.py
+++ celery_app.py
@@ -9,6 +9,5 @@ broker_url = f'amqp://{BROKER_DEFAULT_USER}:{BROKER_DEFAULT_PASSWORD}@{BROKER_HO
 
 celeryapp = Celery("celery_app", broker=broker_url, backend='rpc://')
 celeryapp.conf.imports = [
-    'algorithms.template_worker',
     'grooming.grooming_worker'
 ]
