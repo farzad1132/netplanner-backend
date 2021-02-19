5183bbd4951db29f447466be1412f3f8f0581eb4
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-02-19 22:01:23 +0330

    added shared user list test

--- tests/test_sharing/test_sharing.py
+++ tests/test_sharing/test_sharing.py
@@ -42,6 +42,15 @@ def test_share_pt_v2_0_0():
     respones = client.post(PREFIX + "/v2.0.0/sharing/physical_topology/add", headers=HEADER, json=test_double)
     assert respones.status_code == 200
 
+    # checking if user is in access list or not
+    response = client.get(PREFIX + "/v2.0.0/sharing/physical_topology/users", headers=HEADER, params={"pt_id": test_double["pt_id"]})
+    assert response.status_code == 200
+    flag = False
+    for item in response.json():
+      if id == item["user_id"]:
+        flag = True
+    assert flag
+
     # creating credentials for new user
     head = {"Content-Type": "application/x-www-form-urlencoded",
             "accept": "application/json"}
@@ -60,6 +69,16 @@ def test_share_pt_v2_0_0():
     # removing access user
     respones = client.post(PREFIX + "/v2.0.0/sharing/physical_topology/remove", headers=HEADER, json=test_double)
     assert respones.status_code == 200
+
+    # checking if user is in access list or not
+    response = client.get(PREFIX + "/v2.0.0/sharing/physical_topology/users", headers=HEADER, params={"pt_id": test_double["pt_id"]})
+    assert response.status_code in [404, 200]
+    if response.status_code == 200:
+      flag = False
+      for item in response.json():
+        if id == item["user_id"]:
+          flag = True
+      assert not flag
 
     # checking new user access
     respones = client.get(PREFIX + "/v2.0.0/physical_topologies/", headers=header, params={"id":"1"})
@@ -86,6 +105,15 @@ def test_share_tm_v2_0_0():
     respones = client.post(PREFIX + "/v2.0.0/sharing/traffic_matrix/add", headers=HEADER, json=test_double)
     assert respones.status_code == 200
 
+    # checking if user is in access list or not
+    response = client.get(PREFIX + "/v2.0.0/sharing/traffic_matrix/users", headers=HEADER, params={"tm_id": test_double["tm_id"]})
+    assert response.status_code == 200
+    flag = False
+    for item in response.json():
+      if id == item["user_id"]:
+        flag = True
+    assert flag
+
     # creating credentials for new user
     head = {"Content-Type": "application/x-www-form-urlencoded",
             "accept": "application/json"}
@@ -104,6 +132,16 @@ def test_share_tm_v2_0_0():
     # removing access
     respones = client.post(PREFIX + "/v2.0.0/sharing/traffic_matrix/remove", headers=HEADER, json=test_double)
     assert respones.status_code == 200
+
+    # checking if user is in access list or not
+    response = client.get(PREFIX + "/v2.0.0/sharing/traffic_matrix/users", headers=HEADER, params={"tm_id": test_double["tm_id"]})
+    assert response.status_code in [404, 200]
+    if response.status_code == 200:
+      flag = False
+      for item in response.json():
+        if id == item["user_id"]:
+          flag = True
+      assert not flag
 
     # checking new user access
     respones = client.get(PREFIX + "/v2.0.0/traffic_matrices/", headers=header, params={"id":"1"})
@@ -130,6 +168,15 @@ def test_share_project_v2_0_0():
     respones = client.post(PREFIX + "/v2.0.0/sharing/project/add", headers=HEADER, json=test_double)
     assert respones.status_code == 200
 
+    # checking if user is in access list or not
+    response = client.get(PREFIX + "/v2.0.0/sharing/project/users", headers=HEADER, params={"project_id": test_double["project_id"]})
+    assert response.status_code == 200
+    flag = False
+    for item in response.json():
+      if id == item["user_id"]:
+        flag = True
+    assert flag
+
     # creating credentials for new user
     head = {"Content-Type": "application/x-www-form-urlencoded",
             "accept": "application/json"}
@@ -148,6 +195,16 @@ def test_share_project_v2_0_0():
     # removing access
     respones = client.post(PREFIX + "/v2.0.0/sharing/project/remove", headers=HEADER, json=test_double)
     assert respones.status_code == 200
+
+    # checking if user is in access list or not
+    response = client.get(PREFIX + "/v2.0.0/sharing/project/users", headers=HEADER, params={"project_id": test_double["project_id"]})
+    assert response.status_code in [404, 200]
+    if response.status_code == 200:
+      flag = False
+      for item in response.json():
+        if id == item["user_id"]:
+          flag = True
+      assert not flag
 
     # checking new user access
     respones = client.get(PREFIX + "/v2.0.0/projects", headers=header, params={"id":"1"})
