e0b93a4f2c0374e7fb5a5c8ff1da8bb932feaff4
m-ayaz <m-ayaz@github.com>
2021-02-25 00:43:42 +0330

    Update schemas.py

--- lookup_table/schemas.py
+++ lookup_table/schemas.py
@@ -4,21 +4,21 @@ from typing import Optional, Dict, List, Tuple
 from rwa.schemas import ModulationType
 
 class LookUpTableLink(BaseModel):
-    alpha: float = Field(..., ge=0)
-    beta2: float = Field(..., ge=0)
-    gamma: float = Field(..., ge=0)
-    lspan: float = Field(..., ge=0)
-    nspan: int = Field(..., ge=1)
+    alpha: float = Field(0.2/4.343e3, ge=0)
+    beta2: float = Field(-21e-27, ge=0)
+    gamma: float = Field(1.3e-3, ge=0)
+    lspan: float = Field(100e3, ge=0)
+    nspan: int = Field(1, ge=1)
     amp_gain: float = Field(ge=1)
-    amp_nf: float 
+    amp_nf: float
 
 class LookUpTableIn(BaseModel):
     """
         keys for **links** are links source and destination
     """
     links: Dict[Tuple[str, str], LookUpTableLink]
-    symbol_rate: float = Field(..., ge=0)
-    channel_bandwith: float = Field(..., ge=0)
+    symbol_rate: float = Field(32e9, ge=0)
+    channel_bandwith: float = Field(32e9, ge=0)
     _nmc: float = Field(1e6, ge=1000)
     printlog: bool = False
     max_num_lambda: int = Field(10, ge=1)
