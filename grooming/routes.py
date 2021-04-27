c8afcd9fd34df1b35a85ae1b9bc83e2a95bc2720
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-04-27 21:49:30 +0430

    adding basic adv grooming method updating old grooming path

--- grooming/routes.py
+++ grooming/routes.py
@@ -1,3 +1,4 @@
+from grooming.adv_grooming.schemas import AdvGroomingForm
 from fastapi import APIRouter, Depends, HTTPException
 from typing import Optional, List
 from grooming.schemas import (GroomingForm, GroomingId, GroomingCheck, GroomingIdList, 
@@ -8,7 +9,7 @@ from users.schemas import User
 from projects.utils import GetProject
 from projects.schemas import ProjectSchema
 from clusters.utils import cluster_list_to_cluster_dict
-from grooming.grooming_worker import grooming_task
+from grooming.grooming_worker import adv_grooming_worker, grooming_task
 from models import ClusterModel
 from traffic_matrix.schemas import TrafficMatrixDB
 from physical_topology.schemas import PhysicalTopologyDB
@@ -24,7 +25,8 @@ get_project_mode_get = GetProject()
 
 # SHARE access is only for managers so we can use it for checking authorization in running algorithm mode
 get_project_mode_share = GetProject(mode="SHARE")
-@grooming_router.post("/v2.0.0/algorithms/grooming/start/automatic", status_code=201, response_model=GroomingId)
+@grooming_router.post("/v2.0.0/algorithms/grooming/start/automatic", status_code=201, response_model=GroomingId,
+        deprecated=True)
 def start_automatic(project_id: str, grooming_form: GroomingForm,
                     user: User = Depends(get_current_user),
                     db: Session = Depends(get_db)):
@@ -68,6 +70,75 @@ def start_automatic(project_id: str, grooming_form: GroomingForm,
                                                 clusters=clusters)
     db.add(grooming_register)
     db.commit()
+    return {"grooming_id": task.id}
+
+@grooming_router.post("/v2.0.1/algorithms/grooming/automatic/end_to_end", status_code=201, response_model=GroomingId)
+def start_end_to_end(project_id: str, grooming_form: GroomingForm,
+                    user: User = Depends(get_current_user),
+                    db: Session = Depends(get_db)):
+    """
+        starting automatic grooming algorithm
+    """
+
+    # fetching project and traffic matrix
+    project_db = ProjectSchema.from_orm(get_project_mode_share(id=project_id, user=user, db=db)).dict()
+    traffic_matrix = project_db["traffic_matrix"]
+    physical_topology = project_db["physical_topology"]
+
+    # fetching clusters
+    clusters = db.query(ClusterModel).filter_by(project_id=project_id,
+                                pt_version=project_db["current_pt_version"], 
+                                pt_id=project_db["physical_topology"]["id"],
+                                is_deleted=False).all()
+    
+    # converting cluster_list to cluster_dict
+    cluster_dict = cluster_list_to_cluster_dict(cluster_list=clusters)
+    clusters = ClusterDict.parse_obj(cluster_dict).dict()
+    # starting algorithm
+    task = grooming_task.delay( traffic_matrix=TrafficMatrixDB.parse_obj(traffic_matrix).dict(),
+                                mp1h_threshold=grooming_form.mp1h_threshold,
+                                clusters=clusters, 
+                                Physical_topology=PhysicalTopologyDB.parse_obj(physical_topology).dict())
+    if not clusters["clusters"]:
+        with_clustering = False
+    else:
+        with_clustering = True
+
+    grooming_register = GroomingRegisterModel(  id=task.id,
+                                                project_id=project_id,
+                                                pt_id=project_db["physical_topology"]["id"],
+                                                tm_id=project_db["traffic_matrix"]["id"],
+                                                pt_version=project_db["physical_topology"]["version"],
+                                                tm_version=project_db["traffic_matrix"]["version"],
+                                                form=grooming_form.dict(),
+                                                manager_id=user.id,
+                                                with_clustering=with_clustering,
+                                                clusters=clusters)
+    db.add(grooming_register)
+    db.commit()
+    return {"grooming_id": task.id}
+
+@grooming_router.post("/v2.0.1/algorithms/grooming/automatic/advanced", status_code=201, response_model=GroomingId)
+def start_adv_grooming(project_id: str, grooming_form: AdvGroomingForm,
+                    user: User = Depends(get_current_user),
+                    db: Session = Depends(get_db)):
+    
+    """
+        starting advanced grooming algorithm
+    """
+
+    # fetching project and traffic matrix
+    project_db = ProjectSchema.from_orm(get_project_mode_share(id=project_id, user=user, db=db)).dict()
+    traffic_matrix = project_db["traffic_matrix"]
+    physical_topology = project_db["physical_topology"]
+
+    task = adv_grooming_worker.delay(
+        pt=physical_topology,
+        tm=traffic_matrix,
+        multiplex_threshold=grooming_form.multiplex_threshold,
+        line_rate=grooming_form.line_rate
+    )
+
     return {"grooming_id": task.id}
 
 @grooming_router.post("/v2.0.0/algorithms/grooming/check", status_code=200, response_model=List[GroomingCheck])
