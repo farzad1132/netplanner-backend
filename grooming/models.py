582a468f9c7e5a286165fdce18f723697e2d2cf6
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-04-28 14:45:14 +0430

    AdvGrooming model added

--- grooming/models.py
+++ grooming/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import uuid
 from sqlalchemy.orm import relationship
 from database import base
-from sqlalchemy import Boolean, Integer, String, ForeignKey, Column, DateTime
+from sqlalchemy import Boolean, Integer, String, ForeignKey, Column, DateTime, Float
 
 class GroomingRegisterModel(base):
     __tablename__ = "GroomingRegister"
@@ -57,3 +57,28 @@ class GroomingModel(base):
     def __repr__(self):
         return  f"Grooming(id={self.id}, pt_id={self.pt_id}, tm_id={self.tm_id},"\
                 f" pt_version={self.pt_version}, tm_version={self.tm_version}, manager_id={self.manager_id})"
+
+class AdvGroomingModel(base):
+    __tablename__ = "AdvGrooming"
+    __table_args__ = {'extend_existing': True}
+
+    # generall information
+    id = Column("id", String, primary_key=True)
+    project_id = Column("project_id", String, nullable=False)
+    pt_id = Column("pt_id", String, nullable=False)
+    tm_id = Column("tm_id", String, nullable=False)
+    pt_version = Column("pt_version", Integer, nullable=False)
+    tm_version = Column("tm_version", Integer, nullable=False)
+    form = Column("form", JSON, nullable=False)
+    manager_id = Column(String, ForeignKey("User.id"))
+    manager = relationship("UserModel", back_populates="grooming_algorithms")
+    start_date = Column(DateTime, default=datetime.utcnow)
+    end_date = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    is_finished = Column("is_finished", Boolean, nullable=False, default=False)
+    is_deleted = Column("is_deleted", Boolean, nullable=False, default=False)
+
+    # actual result
+    connections = Column("connections", JSON, nullable=False)
+    lambda_link = Column("lambda_link", Integer, nullable=False)
+    average_lambda_capacity_usage = Column("average_lambda_capacity_usage", Float, nullable=False)
+    lightpaths = Column("lightpaths", JSON, nullable=False)
