cf060c1bc3ab010817eaabb1dcf32cf803191e63
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-05-12 14:55:51 +0430

    utils added to grooming

---
+++ grooming/utils.py
@@ -0,0 +1,7 @@
+from clusters.schemas import ClusterDict
+from fastapi import HTTPException
+
+def check_one_gateway_clusters(clusters: ClusterDict):
+    for id, cluster in clusters.dict().items():
+        if len(cluster['data']['gateways']) != 1:
+            raise HTTPException(status_code=400, detail="only single gateway clusters are currently supported")
