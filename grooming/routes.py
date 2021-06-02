270707a5d382b03c040f493c7aaa9564bd161f85
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-02 17:43:25 +0430

    added manual grooming api

--- grooming/routes.py
+++ grooming/routes.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 from enum import Flag
+from uuid import uuid4
 from grooming.utils import check_one_gateway_clusters
 from grooming.adv_grooming.schemas import AdvGroomingDBOut, AdvGroomingForm
 from fastapi import APIRouter, Depends, HTTPException
 from typing import Optional, List, Union
 from grooming.schemas import (GroomingAlgorithm, GroomingForm, GroomingId, GroomingCheck, GroomingIdList, 
-    GroomingInformation, GroomingDBOut, FailedGroomingInfo)
+    GroomingInformation, GroomingDBOut, FailedGroomingInfo, ManualGroomingDB)
 from sqlalchemy.orm import Session
 from dependencies import get_db, get_current_user
 from users.schemas import User
@@ -125,6 +127,43 @@ def start_end_to_end(project_id: str, grooming_form: GroomingForm,
     db.add(grooming_register)
     db.commit()
     return {"grooming_id": task.id}
+
+@grooming_router.post("/v2.0.0/algorithms/grooming/manual/end_to_end", status_code=201,
+                    response_model=GroomingId)
+def start_manual_grooming(project_id: str, manual_grooming: ManualGroomingDB,
+                    user: User = Depends(get_current_user),
+                    db: Session = Depends(get_db)):
+    """
+        getting manual grooming result from frontend
+    """
+
+    # fetching project and traffic matrix
+    project_db = ProjectSchema.from_orm(get_project_mode_share(id=project_id, user=user, db=db)).dict()
+    traffic_matrix = project_db["traffic_matrix"]
+    physical_topology = project_db["physical_topology"]
+
+    id = uuid4().hex
+    grooming_result = GroomingModel(
+        id=id,
+        project_id=project_id,
+        pt_id=project_db["physical_topology"]["id"],
+        tm_id=project_db["traffic_matrix"]["id"],
+        pt_version=project_db["physical_topology"]["version"],
+        tm_version=project_db["traffic_matrix"]["version"],
+        form=manual_grooming.form.dict(),
+        manager_id=user.id,
+        start_date=datetime.now(),
+        clusters={},
+        is_finished=True,
+        algorithm=GroomingAlgorithm.end_to_end,
+        traffic=manual_grooming.traffic.dict(),
+        service_devices=manual_grooming.service_devices.dict()
+    )
+
+    db.add(grooming_result)
+    db.commit()
+
+    return {"grooming_id": id}
 
 @grooming_router.post("/v2.0.1/algorithms/grooming/automatic/advanced", status_code=201,
                     response_model=GroomingId)
