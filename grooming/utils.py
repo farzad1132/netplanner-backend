d19ef4ada466b39211640a8e30b0fdd5c0fc5f2a
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-02 17:59:57 +0430

    updated, code style in grooming.utils

--- grooming/utils.py
+++ grooming/utils.py
@@ -1,7 +1,9 @@
+from grooming.schemas import ManualGroomingDB
 from clusters.schemas import ClusterDict
 from fastapi import HTTPException
 
-def check_one_gateway_clusters(clusters: ClusterDict):
+def check_one_gateway_clusters(clusters: ClusterDict) -> None:
     for id, cluster in clusters['clusters'].items():
         if len(cluster['data']['gateways']) != 1:
-            raise HTTPException(status_code=400, detail="only single gateway clusters are currently supported")
+            raise HTTPException(status_code=400, 
+                    detail="only single gateway clusters are currently supported")
