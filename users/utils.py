8939bda7d21f47fe53f28272fedf8708cb1bc9e3
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-07 20:25:55 +0430

    added user utils doc

--- users/utils.py
+++ users/utils.py
@@ -1,3 +1,7 @@
+"""
+    This module contains user related utilities
+"""
+
 import smtplib, ssl, os
 from fastapi import HTTPException
 from fastapi import Request
@@ -14,7 +18,16 @@ context = ssl.create_default_context()
 file_loader = FileSystemLoader('static')
 env = Environment(loader=file_loader)
 
-def send_mail(token: str, rcv_mail: str, request: Request):
+def send_mail(token: str, rcv_mail: str, request: Request) -> None:
+    """
+        This functions sends email to user, it usage is for confirmation and registration
+
+        :param token: token that gets included in confirmation link
+        :param rcv_mail: recipient email
+        :param request: request object used by templating engine for creating HTML
+
+        .. note:: This function might raise `HTTPException` with code 500
+    """
     try:
         server = smtplib.SMTP(SERVER, PORT)
         server.starttls(context=context)
@@ -32,7 +45,13 @@ def send_mail(token: str, rcv_mail: str, request: Request):
     finally:
         server.quit()
 
-def create_message(token: str, request: Request):
+def create_message(token: str, request: Request) -> bytes:
+    """
+        This function generates email including HTML
+
+        :param token: token that gets included in confirmation link
+        :param request: request object used by templating engine for creating HTML
+    """
     url = request.url_for('validate_email', **{"token":token})
     template = env.get_template("confirm_email.html")
     message = template.render(url=url)
