6263b849c80dc4cfbfbbc2fae07c8d79eaea518e
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-08 13:04:31 +0430

    added sharing schema doc

--- sharings/schemas.py
+++ sharings/schemas.py
@@ -1,5 +1,13 @@
+"""
+    This schema contains sharing schemas
+"""
+
 from pydantic import BaseModel
 
 class ShareRecord(BaseModel):
+    """
+        This schema represents a share record
+    """
+    
     user_id: str
     username: str
