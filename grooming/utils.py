5149ade9786a8f2c420f4ec3607ff538670b0990
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-09 20:55:15 +0430

    added grooming utils doc

--- grooming/utils.py
+++ grooming/utils.py
@@ -1,7 +1,16 @@
+"""
+    This module contains grooming related utilities
+"""
+
 from clusters.schemas import ClusterDict
 from fastapi import HTTPException
 
-def check_one_gateway_clusters(clusters: ClusterDict):
+def check_one_gateway_clusters(clusters: ClusterDict) -> None:
+    """
+        This function whether a given cluster has single gateway or not (if not it raises `HTTPException` with code `400`)
+
+        :param clusters: collection of cluster to check
+    """
     for id, cluster in clusters['clusters'].items():
         if len(cluster['data']['gateways']) != 1:
             raise HTTPException(status_code=400, detail="only single gateway clusters are currently supported")
