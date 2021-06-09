29de30368d0e76e89031c1dbf5e89a9906f46b0d
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-09 22:35:47 +0430

    updated doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Farzad Mohammadi'
 author = 'Farzad Mohammadi'
 
 # The full version, including alpha/beta/rc tags
-release = '1'
+# release = '1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,8 +47,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+# html_theme = 'alabaster'
+html_theme = 'sphinx_material'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
