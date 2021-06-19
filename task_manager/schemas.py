91d2bdf88078b0a8a726c27572a62a0c8abc5585
dastgheib <sma.dastgheib@gmail.com>
2021-06-19 21:24:42 +0430

    comments added

--- task_manager/schemas.py
+++ task_manager/schemas.py
@@ -3,18 +3,37 @@ from enum import Enum
 from typing import Optional, Dict, List
 
 class BaseTaskID(BaseModel):
+    """ Basic information about each task. 
+    
+    Currently only includes the id of each task. 
+    """
     id: str
 
 class MultiprocessTaskID(BaseModel):
+    """
+    Schema for the IDs of multiprocess tasks.
+
+    By multiprocess tasks, we mean tasks in a similar celery group.
+
+    :param task_level: the position of the multiprocess group in the chain of tasks.
+    :param task_number: number of total steps in the tasks.
+    :param task_id_list: list of the IDs of tasks inside the group.
+    """
     task_level: int
     task_number: int = Field(1, ge=0)
     task_id_list: List[BaseTaskID]
 
 class ChainTaskID(BaseModel):
+    """ The schema for storing a sequence of task ids used for progress report.
+
+    **chain_info** keys are **task level**.
+    """
     chain_id: str
     chain_info: Dict[int, MultiprocessTaskID]
 
 class ChainStatus(BaseModel):
+    """ Status of tasks in a chain.
+    """
     class StateType(str, Enum):
         PENDING = "PENDING"
         STARTED = "STARTED"
@@ -31,6 +50,8 @@ class ChainStatus(BaseModel):
     success: int = Field(0, ge=0)
 
 class ChainProgressReport(BaseModel):
+    """ The result of progress report.
+    """
     id: str
     current_stage_info: str
     status: ChainStatus
