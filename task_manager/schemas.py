a0b41ac5ca8891f76fd1c86e61929066d870d198
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2021-06-23 20:40:06 +0430

    edit style of task_manager.schemas

--- task_manager/schemas.py
+++ task_manager/schemas.py
@@ -1,38 +1,46 @@
-from pydantic import BaseModel, Field
 from enum import Enum
-from typing import Optional, Dict, List
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
 
 class BaseTaskID(BaseModel):
-    """ Basic information about each task. 
-    
-    Currently only includes the id of each task. 
+    """ 
+        Basic information about each task. 
+        
+        Currently only includes the id of each task. 
     """
     id: str
 
 class MultiprocessTaskID(BaseModel):
     """
-    Schema for the IDs of multiprocess tasks.
+        Schema for the IDs of multiprocess tasks.
 
-    By multiprocess tasks, we mean tasks in a similar celery group.
+        By multiprocess tasks, we mean tasks in a similar celery group.
 
-    :param task_level: the position of the multiprocess group in the chain of tasks.
-    :param task_number: number of total steps in the tasks.
-    :param task_id_list: list of the IDs of tasks inside the group.
+        :param task_level: the position of the multiprocess group in the chain of tasks.
+        :param task_number: number of total steps in the tasks.
+        :param task_id_list: list of the IDs of tasks inside the group.
     """
     task_level: int
     task_number: int = Field(1, ge=0)
     task_id_list: List[BaseTaskID]
 
 class ChainTaskID(BaseModel):
-    """ The schema for storing a sequence of task ids used for progress report.
+    """ 
+        The schema for storing a sequence of task ids used for progress report.
 
-    **chain_info** keys are **task level**.
+        **chain_info** keys are **task level**.
     """
     chain_id: str
     chain_info: Dict[int, MultiprocessTaskID]
 
+    class Config:
+        orm_mode = True
+
 class ChainStatus(BaseModel):
-    """ Status of tasks in a chain.
+    """ 
+        Status of tasks in a chain.
     """
     class StateType(str, Enum):
         PENDING = "PENDING"
@@ -50,7 +58,8 @@ class ChainStatus(BaseModel):
     success: int = Field(0, ge=0)
 
 class ChainProgressReport(BaseModel):
-    """ The result of progress report.
+    """ 
+        The result of progress report.
     """
     id: str
     current_stage_info: str
